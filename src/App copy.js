7912bc21c2ad9db0a451ce46363bebb29fc1376a
hwayoung lee <hylee13ee@gmail.com>
2024-03-12 23:05:56 +0900

    [test] PRACTICE MOVIE APP

---
+++ src/App copy.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+
+function Hello(){
+  function byFn() {
+    console.log("bye :(");;
+  }
+
+  function hiFn() {
+    console.log("created :)");
+    return byFn();
+  }
+
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  },[]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
+  return (
+    <div>
+      {showing ? <Hello/>: null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { useEffect, useState } from "react";
-
-
-function Hello(){
-  function byFn() {
-    console.log("bye :(");;
-  }
-
-  function hiFn() {
-    console.log("created :)");
-    return byFn();
-  }
-
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  },[]);
-  return <h1>Hello</h1>;
-}
+import {useEffect, useState} from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectMoney, setSelectMoney] = useState([]);
+  const onChange =(event) => setSelectMoney(event.target.value);
+
+  useEffect (() =>{
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => 
+      response.json()
+    ).then(json => {
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
 
   return (
     <div>
-      {showing ? <Hello/>: null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+    <input 
+      onChange={onChange}
+      value={selectMoney}
+      type="number"
+      placeholder="Write your USD"
+    />
+    {loading ? <strong>Loading...</strong> : <select>
+      {coins.map((coin) => <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+      {coins.map((coin) => <option>{coin.name} ({coin.symbol}): 
+      {selectMoney / coin.quotes.USD.price} {coin.symbol}</option>)}
+    </select> 
+    }
     </div>
   );
+  
 }
 
 export default App;
